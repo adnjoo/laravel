2c8a8e1d2b1fc76299f62fac23d29ebcdf07aab5
Andrew Njoo <adnjoo@gmail.com>
2024-09-14 23:00:07 -0500

    📲

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -134,6 +134,12 @@ export default function Authenticated({ header, children }) {
             >
               Dashboard
             </ResponsiveNavLink>
+            <ResponsiveNavLink
+              href={route('chirps.index')}
+              active={route().current('chirps.index')}
+            >
+              Chirps
+            </ResponsiveNavLink>
           </div>
 
           <div className='pt-4 pb-1 border-t border-gray-200'>
